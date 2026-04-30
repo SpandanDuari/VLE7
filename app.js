4358d082b50183aa6d5d1ce26e11cc55abeb9d9c
Spandan Duari <spandanduari@gmail.com>
2026-04-30 15:48:39 +0530

    adad

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>CI/CD Demo by spandan</h1>
+    <p>Sec: D1</p>
+    <p>Reg No. - RA2311003010207</p>
+    <p>Hostname: ${os.hostname()}</p>
+    <p>Time: ${new Date()}</p>
+  `);
+});
+
+app.get('/health', (req, res) => res.send("OK"));
+
+app.listen(3000, () => console.log("Running"));
