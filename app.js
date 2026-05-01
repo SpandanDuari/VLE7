86ff488a72481920d433599484471aaccf61a187
Spandan Duari <137663824+SpandanDuari@users.noreply.github.com>
2026-05-01 15:12:55 +0530

    Update header in CI/CD demo response

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>CI/CD Demo by spandan</h1>
+    <h1>CI/CD Demo</h1>
     <p>Sec: D1</p>
     <p>Reg No. - RA2311003010207</p>
     <p>Hostname: ${os.hostname()}</p>
